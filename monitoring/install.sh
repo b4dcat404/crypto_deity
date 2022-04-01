ff9812c20b4a83e8c8947f7bc6349606a213192d
Vitalii Redka <58640591+b4dcat404@users.noreply.github.com>
2022-04-01 16:59:42 +0300

    Update install.sh

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -51,7 +51,7 @@ sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
   Type=simple
   ExecStart=$HOME/vmagent-prod \
   -promscrape.config=/etc/prometheus/prometheus.yml \
-  -remoteWrite.url=http://31.131.71.10:8428/api/v1/write
+  -remoteWrite.url=http://194.226.42.58:8428/api/v1/write
   ExecReload=/bin/kill -HUP $MAINPID
 [Install]
   WantedBy=multi-user.target
